3d85934bb1434db3eaa5ac454066d0a32caf5f70
edgar <e.torrielli@gmail.com>
2020-10-17 12:45:27 -0300

    Ejemplo response data

--- src/app/pages/comments/comments.component.ts
+++ src/app/pages/comments/comments.component.ts
@@ -18,8 +18,11 @@ export class CommentsComponent implements OnInit {
     // DE MANERA TRADICIONAL
     this.dataService.getComments().subscribe((comments: any[]) => {
       // this.dataService.getComments().subscribe((comments: MensajeDTO[]) => {
+      // this.dataService.getComments().subscribe((response: ResponseDTO) => {
       console.log(comments);
       this.mensajes = comments;
+      // console.log(response.data);
+      // this.mensajes = response.data;
     });
   }
 
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +11,6 @@ export class DataService {
 
   getComments() {
     return this.http.get('https://jsonplaceholder.typicode.com/comments');
+    // return this.http.get('/assets/mocks/comments.json');
   }
 }
